0de5368488f88a0d4608ad2d1ea96f412e2006a6
sakshi-parmar2004 <140688243+sakshi-parmar2004@users.noreply.github.com>
2023-11-18 19:33:07 +0530

    Create Sdata.jsx
    This is a component for the Netflix index.js file

---
+++ Sdata.jsx
@@ -0,0 +1,39 @@
+const Sdata=[
+    {
+        image:"https://wallpapercave.com/wp/wp4969727.jpg",
+        title:"Stranger Things",
+        name:"Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80057281"
+    },
+    {
+        image:"https://flxt.tmsimg.com/assets/p18344089_b_v13_aa.jpg",
+        title:"13 reasons why",
+        name:"Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80117470"
+    },
+    {
+        image:"https://tse3.mm.bing.net/th?id=OIP.veSy35qv1JdbeYdbF7KrywHaDt&pid=Api&P=0&h=180",
+        title:"Wednesday",
+        name:"Netflix Original Series",
+        link:"https://www.netflix.com/in/title/81231974"
+    },
+    {
+        image:"https://www.denofgeek.com/wp-content/uploads/2020/12/bridgerton-netflix-review.jpg?fit=3208%2C2405",
+        title:"Bridgerton",
+        name:"Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80232398"
+    },
+    {
+        image:"https://tse2.mm.bing.net/th?id=OIP.QKHXXju-TKp4YJc04ntrpwHaFj&pid=Api&P=0&h=180",
+        title:"The Haunting of hill house",
+        name:"Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80189221"
+    },
+    {
+        image:"https://cdn.collider.com/wp-content/uploads/2019/01/sex-education-image-1.jpg",
+        title:"Sex Education",
+        name:"Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80197526"
+    },
+]
+export default Sdata;
